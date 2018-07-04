9e40a7eb3a697d97800be49f050fb4950b7abd80
Joel Dahne <joel@dahne.eu>
2018-07-04 14:31:24 +0200

    scripts/regular_triangles.sh: Script to start computations
    A script to start the computations with certain parameters for all
    regular triangles (i.e. triangles with at most one singular corner).

---
+++ scripts/regular_triangles.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Run computations for all regular triangles (at most one singular corner)
+
+# Parameters
+beg="4" # Starting value for N
+step="4" # Steps in N
+end="8" # End value for N
+tol="1e-10" # Tolerance to use in the minimization
+wid="1e-4" # Width of the interval around the initial guess
+prec="53" # Precision to use
+
+args="-b $beg -s $step -e $end -t $tol -w $wid -p $prec"
+dir="data/eigen_"$beg"_"$step"_"$end"_"$tol"_"$wid"_"$prec
+prog="build/particular-solution"
+
+echo $args
+
+mkdir -p $dir
+
+echo "beg = "$beg   > $dir/parameters
+echo "step = "$step >> $dir/parameters
+echo "end = "$end   >> $dir/parameters
+echo "tol = "$tol   >> $dir/parameters
+echo "wid = "$wid   >> $dir/parameters
+echo "prec = "$prec >> $dir/parameters
+
+# This one has some problem at the moment
+#echo "nohup $prog $args -n 3.056691018 -- 3 4 1 3 1 2 &> $dir/3_4_1_3_1_2 &"
+#nohup $prog $args -n 3.056691018 -- 3 4 1 3 1 2 &> $dir/3_4_1_3_1_2 &
+echo "nohup $prog $args -n 3.240902298 -- 2 3 1 3 1 2 &> $dir/2_3_1_3_1_2 &"
+nohup $prog $args -n 3.240902298 -- 2 3 1 3 1 2 &> $dir/2_3_1_3_1_2 &
+echo "nohup $prog $args -n 4.063109028 -- 2 3 1 4 1 2 &> $dir/2_3_1_4_1_2 &"
+nohup $prog $args -n 4.063109028 -- 2 3 1 4 1 2 &> $dir/2_3_1_4_1_2 &
+echo "nohup $prog $args -n 4.143210850 -- 2 3 1 3 1 3 &> $dir/2_3_1_3_1_3 &"
+nohup $prog $args -n 4.143210850 -- 2 3 1 3 1 3 &> $dir/2_3_1_3_1_3 &
+echo "nohup $prog $args -n 4.470604591 -- 3 4 1 4 1 3 &> $dir/3_4_1_4_1_3 &"
+nohup $prog $args -n 4.470604591 -- 3 4 1 4 1 3 &> $dir/3_4_1_4_1_3 &
+echo "nohup $prog $args -n 6.525663100 -- 2 3 1 4 1 4 &> $dir/2_3_1_4_1_4 &"
+nohup $prog $args -n 6.525663100 -- 2 3 1 4 1 4 &> $dir/2_3_1_4_1_4 &
