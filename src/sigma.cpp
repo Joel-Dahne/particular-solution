b6c67880b1c8eab90bcd90ed309ad88b4d3f5fb3
Joel Dahne <joel@dahne.eu>
2018-07-10 16:34:14 +0200

    src/sigma.cpp: Cleanup

--- src/sigma.cpp
+++ src/sigma.cpp
@@ -15,23 +15,6 @@ struct Points {
   int interior;
 };
 
-/* Computes the sigma value corresponding to how good of
- an approximation of the eigenvalue that nu is.
-
- A_arr - Array to store the coefficients of A in, should be allocated
- to have room for (boundary + interior)*N elements.
-
- thetas, phis - Arrays to contain the theta and phi values to use for
- the points. Should be pointers to Arb vectors. Should have length
- boundary+interior.
-
- boundary - Number of points corresponding to the boundary.
- interior - Number of points corresponding to the interior.
-
- N - Number of terms to use in the expansion.
-
- nu - Value of nu to use in the computations
- */
 mpreal sigma(mpfr_t *A_arr, struct Points points, int N, mpfr_t nu, mpfr_t mu0,
              int (*index)(int)) {
   typedef Matrix<mpreal,Dynamic,Dynamic>  MatrixXmp;
@@ -39,13 +22,11 @@ mpreal sigma(mpfr_t *A_arr, struct Points points, int N, mpfr_t nu, mpfr_t mu0,
   A_arr_mpreal = new mpreal[(points.boundary + points.interior)*N];
 
   // Fill A_arr with the coefficients for the matrix A
-  generate_matrix(A_arr, points, N, nu, mu0,
-                  index);
+  generate_matrix(A_arr, points, N, nu, mu0, index);
 
   // Create an array of mpreals with the same values
-  for (int i = 0; i < (points.boundary + points.interior)*N; i++) {
+  for (int i = 0; i < (points.boundary + points.interior)*N; i++)
     A_arr_mpreal[i] = mpreal(A_arr[i]);
-  }
 
   // Create the matrix A from the coefficients
   Map<MatrixXmp> A(A_arr_mpreal, points.boundary + points.interior, N);
@@ -72,9 +53,8 @@ void coefs_sigma(mpfr_t *coefs_arr, mpfr_t *A_arr, struct Points points,
   generate_matrix(A_arr, points, N, nu, mu0, index);
 
   // Create an array of mpreals with the same values
-  for (int i = 0; i < (points.boundary + points.interior)*N; i++) {
+  for (int i = 0; i < (points.boundary + points.interior)*N; i++)
     A_arr_mpreal[i] = mpreal(A_arr[i]);
-  }
 
   // Create the matrix A from the coefficients
   Map<MatrixXmp> A(A_arr_mpreal, points.boundary + points.interior, N);
@@ -84,17 +64,17 @@ void coefs_sigma(mpfr_t *coefs_arr, mpfr_t *A_arr, struct Points points,
   HouseholderQR<MatrixXmp> qr(A);
   Q = qr.householderQ() * Q;
 
-  // Find the smallest singular values
+  // Compute the right singular vector corresponding to the smallest
+  // singular value
   BDCSVD<MatrixXmp> svd(Q.block(0, 0, points.interior, N), ComputeThinV);
 
-  MatrixXmp coefs;
-
-  coefs = qr.solve(Q*(svd.matrixV().col(N-1)));
+  // Compute the coefficients
+  MatrixXmp coefs = qr.solve(Q*(svd.matrixV().col(N-1)));;
 
   coefs /= coefs(0);
 
-  for (int i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++)
     mpfr_set(coefs_arr[i], coefs(i).mpfr_srcptr(), MPFR_RNDN);
-  }
+
   delete [] A_arr_mpreal;
 }
