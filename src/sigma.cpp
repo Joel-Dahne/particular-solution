66475797da3190bc78cab9c17720c6ee7d721c45
Joel Dahne <joel@dahne.eu>
2018-08-10 20:29:34 +0200

    Move minimization for sigma to sigma.cpp

--- src/sigma.cpp
+++ src/sigma.cpp
@@ -36,6 +36,69 @@ mpreal sigma(mpfr_t *A_arr, points_t points, int N, mpfr_t nu, mpfr_t mu0,
   return svd.singularValues()(N-1);
 }
 
+void minimize_sigma(mpfr_t nu, mpfr_t *A_arr, points_t points, int N,
+                    mpfr_t nu_low, mpfr_t nu_upp, mpfr_t mu0, mpreal tol,
+                    int (*index)(int)) {
+  mpfr_t a, b, c, d;
+  mpreal invphi, invphi2, h, yc, yd;
+  int n;
+
+  mpfr_init(a);
+  mpfr_init(b);
+  mpfr_init(c);
+  mpfr_init(d);
+
+  mpfr_set(a, nu_low, MPFR_RNDN);
+  mpfr_set(b, nu_upp, MPFR_RNDN);
+
+  invphi = (sqrt(5) - 1)/2;
+  invphi2 = (3 - sqrt(5))/2;
+
+  h = mpreal(b) - mpreal(a);
+
+  if (h > tol) {
+    n = (int) ceil(log(tol/h)/log(invphi));
+    mpfr_add(c, a, (invphi2*h).mpfr_srcptr(), MPFR_RNDN);
+    mpfr_add(d, a, (invphi*h).mpfr_srcptr(), MPFR_RNDN);
+
+    yc = sigma(A_arr, points, N, c, mu0, index);
+    yd = sigma(A_arr, points, N, d, mu0, index);
+
+    for (int k = 0; k < n; k++) {
+      if (yc < yd) {
+        mpfr_set(b, d, MPFR_RNDN);
+        mpfr_set(d, c, MPFR_RNDN);
+        yd = yc;
+        h = invphi*h;
+        mpfr_add(c, a, (invphi2*h).mpfr_srcptr(), MPFR_RNDN);
+        yc = sigma(A_arr, points, N, c, mu0, index);
+      } else {
+        mpfr_set(a, c, MPFR_RNDN);
+        mpfr_set(c, d, MPFR_RNDN);
+        yc = yd;
+        h = invphi*h;
+        mpfr_add(d, a, (invphi*h).mpfr_srcptr(), MPFR_RNDN);
+        yd = sigma(A_arr, points, N, d, mu0, index);
+      }
+    }
+
+    if (yc < yd) {
+      mpfr_set(b, d, MPFR_RNDN);
+    } else {
+      mpfr_set(a, c, MPFR_RNDN);
+      mpfr_set(d, b, MPFR_RNDN);
+    }
+  }
+
+  mpfr_add(nu, a, b, MPFR_RNDN);
+  mpfr_div_si(nu, nu, 2, MPFR_RNDN);
+
+  mpfr_clear(a);
+  mpfr_clear(b);
+  mpfr_clear(c);
+  mpfr_clear(d);
+}
+
 void coefs_sigma(mpfr_t *coefs_arr, mpfr_t *A_arr, points_t points,
                  int N, mpfr_t nu, mpfr_t mu0, int (*index)(int)) {
   typedef Matrix<mpreal,Dynamic,Dynamic>  MatrixXmp;
--- src/sigma.h
+++ src/sigma.h
@@ -9,6 +9,10 @@ using namespace mpfr;
 mpreal sigma(mpfr_t *A_arr, points_t points, int N, mpfr_t nu, mpfr_t mu0,
              int (*index)(int));
 
+void minimize_sigma(mpfr_t nu, mpfr_t *A_arr, points_t points, int N,
+                    mpfr_t nu_low, mpfr_t nu_upp, mpfr_t mu0, mpreal tol,
+                    int (*index)(int));
+
 void coefs_sigma(mpfr_t *coefs_arr, mpfr_t *A_arr, points_t points,
                  int N, mpfr_t nu, mpfr_t mu0, int (*index)(int));
 
