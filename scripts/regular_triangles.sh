58b44ea23c29f55d1fa9d31c402831ee0392dfb3
Joel Dahne <joel@dahne.eu>
2018-10-02 13:32:28 +0200

    Update scripts for computing enclosures

--- scripts/regular_triangles.sh
+++ scripts/regular_triangles.sh
@@ -6,13 +6,12 @@ beg="4" # Starting value for N
 step="2" # Steps in N
 end="16" # End value for N
 tol="1e-5" # Relative tolerance to use in the minimization
-wid="1e-4" # Width of the interval around the initial guess
-prec="53" # Precision to use
-type="4" # Type of output
+prec="64" # Precision to use
+type="1" # Type of output
 
-args="-b $beg -s $step -e $end -t $tol -w $wid -p $prec -o $type"
-dir="data/enclsures_"$beg"_"$step"_"$end"_"$tol"_"$wid"_"$prec
-prog="build/particular-solution"
+args="-N_beg $beg -N_step $step -N_end $end -tol $tol -prec $prec -o $type"
+dir="data/enclosures_"$beg"_"$step"_"$end"_"$tol"_"$prec
+prog="build/examples/triangles"
 
 echo $args
 
@@ -22,19 +21,18 @@ echo "beg = "$beg   > $dir/parameters
 echo "step = "$step >> $dir/parameters
 echo "end = "$end   >> $dir/parameters
 echo "tol = "$tol   >> $dir/parameters
-echo "wid = "$wid   >> $dir/parameters
 echo "prec = "$prec >> $dir/parameters
 echo "type = "$type >> $dir/parameters
 
-echo "nohup $prog $args -f 2 -n 3.056691018 -- 3 4 1 3 1 2 &> $dir/3_4_1_3_1_2 &"
-nohup $prog $args -f 2 -n 3.056691018 -- 3 4 1 3 1 2 &> $dir/3_4_1_3_1_2 &
-echo "nohup $prog $args -n 3.240902298 -- 2 3 1 3 1 2 &> $dir/2_3_1_3_1_2 &"
-nohup $prog $args -n 3.240902298 -- 2 3 1 3 1 2 &> $dir/2_3_1_3_1_2 &
-echo "nohup $prog $args -f 2 -n 4.063109028 -- 2 3 1 4 1 2 &> $dir/2_3_1_4_1_2 &"
-nohup $prog $args -f 2 -n 4.063109028 -- 2 3 1 4 1 2 &> $dir/2_3_1_4_1_2 &
-echo "nohup $prog $args -h -i -n 4.143210850 -- 2 3 1 3 1 3 &> $dir/2_3_1_3_1_3 &"
-nohup $prog $args -h -i -n 4.143210850 -- 2 3 1 3 1 3 &> $dir/2_3_1_3_1_3 &
-echo "nohup $prog $args -n 4.470604591 -- 3 4 1 4 1 3 &> $dir/3_4_1_4_1_3 &"
-nohup $prog $args -n 4.470604591 -- 3 4 1 4 1 3 &> $dir/3_4_1_4_1_3 &
-echo "nohup $prog $args -h -i -n 6.525663100 -- 2 3 1 4 1 4 &> $dir/2_3_1_4_1_4 &"
-nohup $prog $args -h -i -n 6.525663100 -- 2 3 1 4 1 4 &> $dir/2_3_1_4_1_4 &
+echo "nohup $prog $args -i 0 &> $dir/3_4_1_3_1_2 &"
+nohup $prog $args       -i 0 &> $dir/3_4_1_3_1_2 &
+echo "nohup $prog $args -i 1 &> $dir/2_3_1_3_1_2 &"
+nohup $prog $args       -i 1 &> $dir/2_3_1_3_1_2 &
+echo "nohup $prog $args -i 2 &> $dir/2_3_1_4_1_2 &"
+nohup $prog $args       -i 2 &> $dir/2_3_1_4_1_2 &
+echo "nohup $prog $args -i 3 &> $dir/2_3_1_3_1_3 &"
+nohup $prog $args       -i 3 &> $dir/2_3_1_3_1_3 &
+echo "nohup $prog $args -i 4 &> $dir/3_4_1_4_1_3 &"
+nohup $prog $args       -i 4 &> $dir/3_4_1_4_1_3 &
+echo "nohup $prog $args -i 5 &> $dir/2_3_1_4_1_4 &"
+nohup $prog $args       -i 5 &> $dir/2_3_1_4_1_4 &
--- scripts/singular_triangles.sh
+++ scripts/singular_triangles.sh
@@ -6,13 +6,12 @@ beg="4" # Starting value for N
 step="2" # Steps in N
 end="16" # End value for N
 tol="1e-5" # Relative tolerance to use in the minimization
-wid="1e-4" # Width of the interval around the initial guess
-prec="53" # Precision to use
-type="4" # Type of output
+prec="64" # Precision to use
+type="1" # Type of output
 
-args="-b $beg -s $step -e $end -t $tol -w $wid -p $prec -o $type"
-dir="data/enclsures_"$beg"_"$step"_"$end"_"$tol"_"$wid"_"$prec
-prog="build/particular-solution"
+args="-N_beg $beg -N_step $step -N_end $end -tol $tol -prec $prec -o $type"
+dir="data/enclosures_"$beg"_"$step"_"$end"_"$tol"_"$prec
+prog="build/examples/triangles"
 
 echo $args
 
@@ -22,15 +21,14 @@ echo "beg = "$beg   > $dir/parameters
 echo "step = "$step >> $dir/parameters
 echo "end = "$end   >> $dir/parameters
 echo "tol = "$tol   >> $dir/parameters
-echo "wid = "$wid   >> $dir/parameters
 echo "prec = "$prec >> $dir/parameters
 echo "type = "$type >> $dir/parameters
 
-echo "nohup $prog $args -n 1.624084509 -- 2 3 3 4 3 4 &> $dir/2_3_3_4_3_4 &"
-nohup $prog $args -n 1.624084509 -- 2 3 3 4 3 4 &> $dir/2_3_3_4_3_4 &
-echo "nohup $prog $args -n 1.825757081 -- 2 3 2 3 2 3 &> $dir/2_3_2_3_2_3 &"
-nohup $prog $args -n 1.825757081 -- 2 3 2 3 2 3 &> $dir/2_3_2_3_2_3 &
-echo "nohup $prog $args -n 2.047890892 -- 2 3 3 4 1 2 &> $dir/2_3_3_4_1_2 &"
-nohup $prog $args -n 2.047890892 -- 2 3 3 4 1 2&> $dir/2_3_3_4_1_2 &
-echo "nohup $prog $args -n 2.150869291 -- 2 3 2 3 1 2 &> $dir/2_3_2_3_1_2 &"
-nohup $prog $args -n 2.150869291 -- 2 3 2 3 1 2 &> $dir/2_3_2_3_1_2 &
+echo "nohup $prog $args -i 6 &> $dir/2_3_3_4_3_4 &"
+nohup $prog $args       -i 6 &> $dir/2_3_3_4_3_4 &
+echo "nohup $prog $args -i 7 &> $dir/2_3_2_3_2_3 &"
+nohup $prog $args       -i 7 &> $dir/2_3_2_3_2_3 &
+echo "nohup $prog $args -i 8 &> $dir/1_2_2_3_3_4 &"
+nohup $prog $args       -i 8 &> $dir/1_2_2_3_3_4 &
+echo "nohup $prog $args -i 9 &> $dir/1_2_2_3_2_3 &"
+nohup $prog $args       -i 9 &> $dir/1_2_2_3_2_3 &
