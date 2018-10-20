cb8af965c4ddb89c7e77fed1da88f493cb39130d
Joel Dahne <joel@dahne.eu>
2018-10-20 10:55:51 +0200

    src/generate-matrix.cpp: Factor out computation of the eigenfunction

--- src/generate-matrix.cpp
+++ src/generate-matrix.cpp
@@ -2,13 +2,36 @@
 
 #include "arb_hypgeom.h"
 
+static void
+eval_eigenfunction(arb_t res, arb_t theta, arb_t phi, arb_t nu, arb_t mu,
+                   slong prec)
+{
+  arb_t tmp;
+  slong prec_local;
+
+  arb_init(tmp);
+
+  arb_cos(tmp, theta, prec);
+  prec_local = prec;
+  do {
+    arb_hypgeom_legendre_p(res, nu, mu, tmp, 0, prec_local);
+    prec_local *=2;
+  } while (!arb_is_finite(res));
+
+  arb_mul(tmp, mu, phi, prec);
+  arb_sin(tmp, tmp, prec);
+
+  arb_mul(res, res, tmp, prec);
+
+  arb_clear(tmp);
+}
+
 void generate_matrix(mpfr_t *A, geom_t geom, points_t points, slong N, arb_t nu,
                      slong prec) {
-  arb_t mu, tmp, res;
-  slong prec_local, n;
+  arb_t mu, res;
+  slong n;
 
   arb_init(mu);
-  arb_init(tmp);
   arb_init(res);
 
   n = points->boundary + points->interior;
@@ -17,34 +40,22 @@ void generate_matrix(mpfr_t *A, geom_t geom, points_t points, slong N, arb_t nu,
     for (slong j = 0; j < N; j++) {
       geom_get_mu(mu, geom, 0, j, prec);
 
-      arb_cos(tmp, points->thetas + i, prec);
-      prec_local = prec;
-      do {
-        arb_hypgeom_legendre_p(res, nu, mu, tmp, 0, prec_local);
-        prec_local *=2;
-      } while (!arb_is_finite(res));
-
-      arb_mul(tmp, mu, points->phis + i, prec);
-      arb_sin(tmp, tmp, prec);
-
-      arb_mul(res, res, tmp, prec);
+      eval_eigenfunction(res, points->thetas + i, points->phis + i, nu, mu,
+                         prec);
 
       arf_get_mpfr(A[j*n + i], arb_midref(res), MPFR_RNDN);
     }
   }
 
   arb_clear(mu);
-  arb_clear(tmp);
   arb_clear(res);
 }
 
 void eigenfunction(arb_ptr res, geom_t geom, arb_ptr coefs, points_t points,
                    slong N, arb_t nu, slong prec) {
-  arb_t mu, tmp, term;
-  slong prec_local;
+  arb_t mu, term;
 
   arb_init(mu);
-  arb_init(tmp);
   arb_init(term);
 
   for (slong i = 0; i < points->boundary + points->interior; i++) {
@@ -52,23 +63,12 @@ void eigenfunction(arb_ptr res, geom_t geom, arb_ptr coefs, points_t points,
     for (slong j = 0; j < N; j++) {
       geom_get_mu(mu, geom, 0, j, prec);
 
-      arb_cos(tmp, points->thetas + i, prec);
-      prec_local = prec;
-      do {
-        arb_hypgeom_legendre_p(term, nu, mu, tmp, 0, prec_local);
-        prec_local *=2;
-      } while (!arb_is_finite(term));
-
-      arb_mul(tmp, mu, points->phis + i, prec);
-      arb_sin(tmp, tmp, prec);
-
-      arb_mul(term, term, tmp, prec);
+      eval_eigenfunction(term, points->thetas + i, points->phis + i, nu, mu, prec);
 
       arb_addmul(res + i, term, coefs + j, prec);
     }
   }
 
   arb_clear(mu);
-  arb_clear(tmp);
   arb_clear(term);
 }
