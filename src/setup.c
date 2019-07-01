c4bcf0526a2d533a6a92e064080840458f871f05
Joel Dahne <joel@dahne.eu>
2019-07-01 16:49:58 +0200

    setup.c: Change the default number of points when plotting sigma

--- src/setup.c
+++ src/setup.c
@@ -8,7 +8,7 @@ particular_solution_opt_default(particular_solution_opt_t options)
   options->N_beg = 4;
   options->N_end = 16;
   options->N_step = 2;
-  options->plot_n = 0;
+  options->plot_n = 100;
   options->output = 0;
   options->output_final = 0;
   options->output_time = 0;
