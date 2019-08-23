707301e03a75c4e341304a758ebc8ed47b6025e6
Joel Dahne <joel@dahne.eu>
2019-08-23 18:12:51 +0200

    Add Taylor expansions and redo the Julia eigenfunction interface
    Adds Taylor expansions of the eigenfunction for Cartesian triangles
    and cleans up the interface for calling eigenfunctions from Julia.

--- src/cartesian/eigenfunction.c
+++ src/cartesian/eigenfunction.c
@@ -2,13 +2,128 @@
 
 #include "arb_hypgeom.h"
 
-/* Compute the value of the basis functions used in the expansion for
- * the eigenfunction at the point given by angle and radii with the
- * paremeters nu and mu.
+/* Compute the Taylor expansion of the Bessel J function evaluated at
+ * the Taylor expansion given by x with parameter nu. The order of the
+ * computed expansion is n - 1, i.e. the number of terms is equal to
+ * n.
  *
- * nu: square root of the eigenvalue
- * mu: corresponds to nu in the standard notation
- */
+ * For the computations it uses ... */
+static void
+bessel_j_series(arb_ptr res, arb_t nu, arb_ptr x, slong len, slong n,
+                slong prec)
+{
+  arb_ptr bessel, tmp_poly;
+  arb_t nu_tmp, bessel_tmp, tmp;
+  slong k;
+
+  bessel = _arb_vec_init(n);
+  tmp_poly = _arb_vec_init(n);
+
+  arb_init(nu_tmp);
+  arb_init(bessel_tmp);
+  arb_init(tmp);
+
+  if (n > 0)
+  {
+    /* a_0 */
+    arb_hypgeom_bessel_j(bessel + 0, nu, x, prec);
+
+    if (n > 1)
+    {
+      /* a_1 */
+      arb_sub_si(nu_tmp, nu, 1, prec);
+      arb_hypgeom_bessel_j(bessel + 1, nu_tmp, x, prec);
+
+      arb_add_si(nu_tmp, nu, 1, prec);
+      arb_hypgeom_bessel_j(bessel_tmp, nu_tmp, x, prec);
+
+      arb_sub(bessel + 1, bessel + 1, bessel_tmp, prec);
+      arb_div_si(bessel + 1, bessel + 1, 2, prec);
+
+      if (n > 2)
+      {
+        /* a_2 */
+        arb_mul_si(bessel + 2, bessel + 0, -2, prec);
+
+        arb_sub_si(nu_tmp, nu, 2, prec);
+        arb_hypgeom_bessel_j(bessel_tmp, nu_tmp, x, prec);
+        arb_add(bessel + 2, bessel + 2, bessel_tmp, prec);
+
+        arb_add_si(nu_tmp, nu, 2, prec);
+        arb_hypgeom_bessel_j(bessel_tmp, nu_tmp, x, prec);
+        arb_add(bessel + 2, bessel + 2, bessel_tmp, prec);
+
+        arb_div_si(bessel + 2, bessel + 2, 8, prec);
+
+        if (n > 3)
+        {
+          /* a_3 */
+          arb_mul_si(bessel + 3, bessel + 1, -6, prec);
+
+          arb_sub_si(nu_tmp, nu, 3, prec);
+          arb_hypgeom_bessel_j(bessel_tmp, nu_tmp, x, prec);
+          arb_add(bessel + 3, bessel + 3, bessel_tmp, prec);
+
+          arb_add_si(nu_tmp, nu, 3, prec);
+          arb_hypgeom_bessel_j(bessel_tmp, nu_tmp, x, prec);
+          arb_sub(bessel + 3, bessel + 3, bessel_tmp, prec);
+
+          arb_div_si(bessel + 3, bessel + 3, 48, prec);
+
+          /* Compute the remaining terms using the recurrence relation */
+          for (slong i = 4; i < n; i++)
+          {
+            k = i - 4;
+
+            /* a_k term*/
+            arb_set(bessel + k + 4, bessel + k);
+
+            /* a_{k + 1} term*/
+            arb_mul_si(tmp, x, 2, prec);
+
+            arb_addmul(bessel + k + 4, bessel + k + 1, tmp, prec);
+
+            /* a_{k + 2} term*/
+            arb_sqr(tmp, x, prec);
+            arb_submul(tmp, nu, nu, prec);
+            arb_add_si(tmp, tmp, k*k + 4*k + 4, prec);
+
+            arb_addmul(bessel + k + 4, bessel + k + 2, tmp, prec);
+
+            /* a_{k + 3} term*/
+            arb_mul_si(tmp, x, 2*k*k + 11*k + 15, prec);
+
+            arb_addmul(bessel + k + 4, bessel + k + 3, tmp, prec);
+
+            /* Factor */
+            arb_sqr(tmp, x, prec);
+            arb_mul_si(tmp, tmp, k*k + 7*k + 12, prec);
+
+            arb_div(bessel + k + 4, bessel + k + 4, tmp, prec);
+            arb_neg(bessel + k + 4, bessel + k + 4);
+          }
+        }
+      }
+    }
+  }
+
+  /* Compose the Taylor series for the Legendre function with that of
+   * z */
+  _arb_vec_set(tmp_poly, x, n);
+  arb_zero(tmp_poly);
+  _arb_poly_compose_series(res, bessel, n, tmp_poly, n, n, prec);
+
+  _arb_vec_clear(bessel, n);
+  _arb_vec_clear(tmp_poly, n);
+
+  arb_clear(nu_tmp);
+  arb_clear(bessel_tmp);
+  arb_clear(tmp);
+}
+
+/* Compute the value of the basis functions used in the expansion for
+ * the eigenfunction at the point given by angle theta and radii r
+ * with the parameters lambda an nu. */
 void
 eigenfunction_basis(arb_t res, arb_t r, arb_t theta, arb_t lambda, arb_t nu,
                     slong prec)
@@ -29,14 +144,44 @@ eigenfunction_basis(arb_t res, arb_t r, arb_t theta, arb_t lambda, arb_t nu,
   arb_clear(tmp);
 }
 
-/* TODO: Implement this*/
+/* Compute the Taylor expansion of the basis functions used in the
+ * expansion for the eigenfunction at the point given by angle theta
+ * and radii r with the parameters lambda an nu. The order of the
+ * computed expansion is n - 1, i.e. the number of terms is equal to
+ * n. */
 void
-eigenfunction_basis_series(arb_ptr res, arb_ptr z, arb_ptr phi, arb_t nu,
-                           arb_ptr mu, slong n, slong prec)
+eigenfunction_basis_series(arb_ptr res, arb_ptr r, arb_ptr theta, arb_t lambda,
+                           arb_ptr nu, slong n, slong prec)
 {
+  arb_ptr tmp1, tmp2;
+  arb_t sqrtlambda;
 
+  tmp1 = _arb_vec_init(n);
+  tmp2 = _arb_vec_init(n);
+
+  arb_init(sqrtlambda);
+
+  arb_sqrt(sqrtlambda, lambda, prec);
+  _arb_vec_scalar_mul(tmp1, r, n, sqrtlambda, prec);
+  bessel_j_series(tmp2, nu, tmp1, n, n, prec);
+
+  _arb_vec_scalar_mul(tmp1, theta, n, nu, prec);
+  _arb_poly_sin_series(tmp1, tmp1, n, n, prec);
+
+  _arb_poly_mullow(res, tmp1, n, tmp2, n, n, prec);
+
+  arb_clear(sqrtlambda);
+
+  /////
+  //bessel_j_series(res, nu, r, n, n, prec);
+
+  _arb_vec_clear(tmp1, n);
+  _arb_vec_clear(tmp2, n);
 }
 
+/* Compute the value of the eigenfunction function given by a sum of N
+ * basis functions with the given coefficients at the point given by
+ * theta and r using the parameter lambda. */
 void
 eigenfunction(arb_t res, geom_t geom, arb_ptr coefs, slong N, arb_t r,
                    arb_t theta, arb_t lambda, slong vertex, slong prec)
@@ -61,11 +206,35 @@ eigenfunction(arb_t res, geom_t geom, arb_ptr coefs, slong N, arb_t r,
   arb_clear(term);
 }
 
-/* TODO: Implement this*/
+/* Compute the Taylor expansion of the eigenfunction function given by
+ * a sum of N basis functions with the given coefficients at the point
+ * given by theta and r using the parameter lambda. The order of the
+ * computed expansion is n - 1, i.e. the number of terms is equal to
+ * n. */
 void
 eigenfunction_series(arb_ptr res, geom_t geom, arb_ptr coefs, slong N,
-                     arb_ptr z, arb_ptr phi, arb_t nu, slong vertex,
+                     arb_ptr r, arb_ptr theta, arb_t lambda, slong vertex,
                      slong n, slong prec)
 {
+  arb_ptr term;
+  arb_t nu;
 
+  term = _arb_vec_init(n);
+
+  arb_init(nu);
+
+  _arb_vec_zero(res, n);
+
+  for (slong i = 0; i < N; i++)
+  {
+    geom_get_mu(nu, geom, vertex, i, prec);
+
+    eigenfunction_basis_series(term, r, theta, lambda, nu, n, prec);
+
+    _arb_vec_scalar_addmul(res, term, n, coefs + i, prec);
+  }
+
+  _arb_vec_clear(term, n);
+
+  arb_clear(nu);
 }
--- src/julia/eigenfunction.jl
+++ src/julia/eigenfunction.jl
@@ -1,3 +1,15 @@
+"""
+Compute the basis eigenfunctions, the approximate eigenfunction is
+given by a linear combination of these. It takes as input a point and
+two parameters.
+
+For the spherical case the point is given by (θ, ϕ) and the parameters
+by ν and μ, where ν corresponds to the eigenvalue and μ is the index
+of the function.
+
+For the Cartesian case the point is given by (r, θ) and the parameters
+by λ and ν, where λ is the eigenvalue and ν the index of the function.
+"""
 function eigenfunction_basis(θ::ArbReal, ϕ::ArbReal, ν::ArbReal, μ::ArbReal)
     res = ArbReal(0)
 
@@ -8,23 +20,77 @@ function eigenfunction_basis(θ::ArbReal, ϕ::ArbReal, ν::ArbReal, μ::ArbReal)
     return res
 end
 
-function eigenfunction_basis(point::ISOSpherical{ArbReal{T}}, ν::ArbReal, μ::ArbReal) where T
+
+function eigenfunction_basis(point::ISOSpherical, ν, μ)
     eigenfunction_basis(point.θ, point.ϕ, ν, μ)
 end
 
-"""
-Compute the basis eigenfunctions for the plane. The input is the same
-as for the spherical case but with different naming conventions. The
-argument names here correspond to the naming in the plane, but it then
-calls a method where the naming corresponds to the sphere.
-"""
-function eigenfunction_basis(point::Polar{ArbReal{T}}, λ::ArbReal, ν::ArbReal) where T
+function eigenfunction_basis(point::Polar, λ, ν)
     eigenfunction_basis(point.r, point.θ, λ, ν)
 end
 
-function eigenfunction(θ::ArbReal, ϕ::ArbReal, ν::ArbReal,
-                       coeffs::Array{ArbReal, 1}, g::GeometrySpherical, vertex::Int = 1)
+"""
+Compute the Taylor expansion of the basis eigenfunctions. It takes as
+input the Taylor expansion of a point and two parameters. The
+parameters are the same as for eigenfunction_basis.
+
+The point is either (θ, ϕ) for the spherical case or (r, θ) for the
+Cartesian case. In both cases they should be the n-th order Taylor
+expansions of the respective values. The output is the computed to the
+same order.
+"""
+function eigenfunction_basis_series(θ::Vector{T}, ϕ::Vector{T},
+                                    ν::ArbReal, μ::ArbReal) where T <: ArbReal
+    @assert length(θ) == length(ϕ)
+    n = length(θ)
+
+    θPtr = _arb_vec_init(n)
+    ϕPtr = _arb_vec_init(n)
+    resPtr = _arb_vec_init(n)
+    unsafe_store_ArbRealPtr!(θPtr, θ)
+    unsafe_store_ArbRealPtr!(ϕPtr, ϕ)
+
+    ccall((:eigenfunction_basis_series, "build/particular_solution"), Cvoid,
+          (Ptr{ArbReal}, Ptr{ArbReal}, Ptr{ArbReal}, Ref{ArbReal}, Ref{ArbReal}, Int, Int),
+          resPtr, θPtr, ϕPtr, ν, μ, n, workingprecision(ν))
+
+    res = unsafe_load_ArbRealPtr(resPtr, n)
+
+    _arb_vec_clear(θPtr, n)
+    _arb_vec_clear(ϕPtr, n)
+    _arb_vec_clear(resPtr, n)
+
+    return res
+end
+
+function eigenfunction_basis_series(point::Vector{T}, ν, μ) where T <: ISOSpherical
+    eigenfunction_basis_series(map(p -> p.θ, point), map(p -> p.ϕ, point), ν, μ)
+end
+
+function eigenfunction_basis_series(point::Vector{T}, ν, μ) where T <: Polar
+    eigenfunction_basis_series(map(p -> p.r, point), map(p -> p.θ, point), ν, μ)
+end
+
+"""
+Compute the approximate eigenfunction. The eigenfunction is given by a
+linear combination of the basis eigenfunction with the given
+coefficients. It takes as input a point, a parameter, the coefficients
+and the geometrical data needed to determine the function.
+
+For the spherical case the point is given by (θ, ϕ) and the parameter
+by ν which corresponds to the eigenvalue. The geometrical information
+is a GeometrySpherical as well as which vertex it should be evaluated
+from.
+
+For the Cartesian case the point is given by (r, θ) and the parameter
+by λ which is the eigenvalue. The geometrical information is a
+GeometryCartesian, the vertex should always be 1 since support for
+other vertices is not implemented.
+"""
+function eigenfunction(θ::ArbReal, ϕ::ArbReal, ν::ArbReal, coeffs::Vector{T},
+                       g::Geometry, vertex::Int = 1) where T <: ArbReal
     res = ArbReal(0)
+
     N = length(coeffs)
     coeffsPtr = _arb_vec_init(N)
     unsafe_store_ArbRealPtr!(coeffsPtr, coeffs)
@@ -39,29 +105,65 @@ function eigenfunction(θ::ArbReal, ϕ::ArbReal, ν::ArbReal,
     return res
 end
 
-function eigenfunction(point::ISOSpherical{ArbReal{T}}, ν::ArbReal,
-                       coeffs::Array{ArbReal, 1}, g::GeometrySpherical, vertex::Int = 1) where T
+function eigenfunction(point::ISOSpherical, ν, coeffs, g::GeometrySpherical,
+                       vertex = 1)
     eigenfunction(point.θ, point.ϕ, ν, coeffs, g, vertex)
 end
 
-function eigenfunction(r::ArbReal, θ::ArbReal, λ::ArbReal,
-                       coeffs::Array{ArbReal, 1}, g::GeometryCartesian)
-    res = ArbReal(0)
+function eigenfunction(point::Polar, λ, coeffs, g::GeometryCartesian)
+    eigenfunction(point.r, point.θ, λ, coeffs, g, 1)
+end
+
+"""
+Compute the Taylor expansion of the approximate eigenfunction. It takes as
+input the Taylor expansion of a point, a parameter, the coefficients
+and the geometrical data needed to determine the function. The
+parameter, the coefficients and geometrical data is the same as for
+eigenfunction.
+
+The point is either (θ, ϕ) for the spherical case or (r, θ) for the
+Cartesian case. In both cases they should be the n-th order Taylor
+expansions of the respective values. The output is the computed to the
+same order.
+"""
+function eigenfunction_series(θ::Vector{T}, ϕ::Vector{T}, ν::ArbReal,
+                              coeffs::Vector{S} where S <: ArbReal, g::Geometry,
+                              vertex::Int = 1) where T <: ArbReal
+    @assert length(θ) == length(ϕ)
+    n = length(θ)
+
+    θPtr = _arb_vec_init(n)
+    ϕPtr = _arb_vec_init(n)
+    resPtr = _arb_vec_init(n)
+    unsafe_store_ArbRealPtr!(θPtr, θ)
+    unsafe_store_ArbRealPtr!(ϕPtr, ϕ)
+
     N = length(coeffs)
     coeffsPtr = _arb_vec_init(N)
     unsafe_store_ArbRealPtr!(coeffsPtr, coeffs)
 
-    ccall((:eigenfunction, "build/particular_solution"), Cvoid,
-          (Ref{ArbReal}, Ref{Geometry}, Ptr{ArbReal}, Int, Ref{ArbReal},
-           Ref{ArbReal}, Ref{ArbReal}, Int, Int),
-          res, g, coeffsPtr, N, r, θ, λ, 0, workingprecision(λ))
+    ccall((:eigenfunction_series, "build/particular_solution"), Cvoid,
+          (Ptr{ArbReal}, Ref{Geometry}, Ptr{ArbReal}, Int, Ptr{ArbReal},
+           Ptr{ArbReal}, Ref{ArbReal}, Int, Int, Int),
+          resPtr, g, coeffsPtr, N, θPtr, ϕPtr, ν, vertex - 1, n, workingprecision(ν))
+
+    res = unsafe_load_ArbRealPtr(resPtr, n)
+
+    _arb_vec_clear(θPtr, n)
+    _arb_vec_clear(ϕPtr, n)
+    _arb_vec_clear(resPtr, n)
 
     _arb_vec_clear(coeffsPtr, N)
 
     return res
 end
 
-function eigenfunction(point::Polar{ArbReal{T}}, λ::ArbReal,
-                       coeffs::Array{ArbReal, 1}, g::GeometryCartesian) where T
-    eigenfunction(point.r, point.θ, λ, coeffs, g)
+function eigenfunction_series(point::Vector{T}, ν, coeffs, g::GeometrySpherical,
+                       vertex = 1) where T <: ISOSpherical
+    eigenfunction_series(map(p -> p.θ, point), map(p -> p.ϕ, point), ν, coeffs, g, vertex)
+end
+
+function eigenfunction_series(point::Vector{T}, λ, coeffs,
+                              g::GeometryCartesian) where T <: Polar
+    eigenfunction_series(map(p -> p.r, point), map(p -> p.θ, point), λ, coeffs, g, 1)
 end
